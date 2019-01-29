b6b2135ede4ee04538adf83f70088baae5ea86c3
ramigueti <rafa.amigueti@gmail.com>
2019-01-30 00:35:18 +0100

    VendignMachine class terminada

--- Interview4/VendingMachine.cpp
+++ Interview4/VendingMachine.cpp
@@ -1,87 +1,104 @@
 #include "VendingMachine.h"
 #include "ItemStack.h"
 
-void VendingMachine::addItemStack(const ItemStack& item_stack)
+void VendingMachine::addItemStack(const std::string& grid_position, const ItemStack& item_stack)
 {
-  m_item_stacks.push_back(item_stack);
+  if(std::regex_match(grid_position, m_grid_regex))
+  {
+    m_item_stacks.insert(std::make_pair(grid_position, item_stack));
+  }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << " ERROR grid position = " << grid_position << " not valid!!" << std::endl;
+  }
 }
 
-void VendingMachine::deleteItemStack(const std::string& title, bool all_ocurrences)
+void VendingMachine::deleteItemStack(const std::string& grid_position)
 {
-  bool finished = false;
- 
-  std::vector<std::vector<ItemStack>::const_iterator> its_to_delete; 
-  for (auto it = m_item_stacks.begin(); it != m_item_stacks.end() && !finished; ++it)
+  if(std::regex_match(grid_position, m_grid_regex))
   {
-    if(it->title() == title)
+    m_item_stacks.erase(grid_position);
+  }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << " ERROR grid position = " << grid_position << " not valid!!" << std::endl;
+  }
+}
+
+void VendingMachine::addMoney(const float& money) const
+{
+  if (money > 0.0)
+  {
+    m_money = m_money + money;
+  }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << " ERROR money negative" << std::endl;
+  }
+}
+
+std::shared_ptr<ItemStack> VendingMachine::queryForItem(const std::string& grid_position) const
+{
+  std::shared_ptr<ItemStack> item = nullptr;
+
+  if(std::regex_match(grid_position, m_grid_regex))
+  {
+    auto it = m_item_stacks.find(grid_position);
+    if (it!=m_item_stacks.end())
     {
-      its_to_delete.push_back(it);
-      if(!all_ocurrences)
+      item.reset(it->second);
+    }
+  }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << " ERROR grid position = " << grid_position << " not valid!!" << std::endl;
+  }
+
+  return item;
+}
+
+void VendingMachine::retrieveItem(const std::string& grid_postion)
+{
+  if(m_money > 0.0)
+  {
+    if(std::regex_match(grid_position, m_grid_regex))
+    {
+      auto it = m_item_stacks.find(grid_position);
+      if (it!=m_item_stacks.end())
       {
-        finished = true;
-      }
-    }
-  }
-  for (auto it_to_delete : its_to_delete)
-  {
-    m_item_stacks.erase(it_to_delete);
-  }
-}
-
-std::vector<ItemStack> VendingMachine::searchByTitle(const std::string& title) const
-{
-  std::vector<ItemStack> item_stacks_found;
-
-  for(const auto& item_stack : m_item_stacks)
-  {
-    if(item_stack.title() == title)
-    {
-      item_stacks_found.push_back(item_stack);
-    }
-  }
-
-  return item_stacks_found;
-}
-
-std::vector<ItemStack> VendingMachine::searchByText(const std::string& text) const
-{
-  std::vector<ItemStack> item_stacks_found;
-
-  for(const auto& item_stack : m_item_stacks)
-  {
-    if(item_stack.text() == text)
-    {
-      item_stacks_found.push_back(item_stack);
-    }
-  }
-
-  return item_stacks_found;
-}
-
-std::vector<ItemStack> VendingMachine::searchByTag(const std::vector<std::string>& tags) const
-{
-  std::vector<ItemStack> item_stacks_found;
-
-  if(!tags.empty())
-  {
-    for(const auto& item_stack : m_item_stacks)
-    {
-      bool found = true;
-      for(auto it = tags.begin(); it != tags.end() && found; ++it)
-      {
-        auto item_stack_tags = item_stack.tags();
-        if(item_stack_tags.find(*it)==item_stack_tags.end())
+        if(it->second.count() >= 1)
         {
-          found = false; 
+          if(m_money >= it->second.cost())
+          {
+            it->second.retreive();
+            m_money = m_money - it->second.cost();
+            std::cout << "pick up " << it->second.title() << std::endl;
+          }
+          else
+          {
+            std::cerr << __PRETTY_FUNCTION__ << " ERROR not money for buy this item" << std::endl;
+          }
+        }
+        else
+        {
+            std::cerr << __PRETTY_FUNCTION__ << " ERROR no stock" << std::endl;
         }
       }
-      if(found)
-      {
-        item_stacks_found.push_back(item_stack);
-      }
+    }
+    else
+    {
+      std::cerr << __PRETTY_FUNCTION__ << " ERROR grid position = " << grid_position << " not valid!!" << std::endl;
     }
   }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << " ERROR not money for buy this item" << std::endl;
+  }
+}
 
-  return item_stacks_found;
+float VendingMachine::returnChange()
+{
+  std::cout << "Your change thanks" << std::endl;
+  return m_money;
 }
 
